40676019fac7ce92700446d830555087614b4073
mohankumargowdaa <india.mohankumar@gmail.com>
2022-05-11 13:52:03 +0530

    updated getData.java file

--- src/test/java/com/vtiger/practice/GetDate.java
+++ src/test/java/com/vtiger/practice/GetDate.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 public class GetDate {
 public static void main(String[] args) {
+	//get the date in required format
 	Date date=new Date();
 SimpleDateFormat f=new SimpleDateFormat("yyyy_MM_dd_HH_mm_sss");
 String date1 = f.format(date);
